979f36b230973859316cac471347ee21a7203c5b
HeizlHazel <hyejee0221@gmail.com>
2025-12-01 19:07:25 +0900

    :white_check_mark: 리액트 로그인 테스트 코드

--- workeezy-react/src/App.jsx
+++ workeezy-react/src/App.jsx
@@ -1,6 +1,29 @@
 import ReviewPage from "./pages/ReviewPage";
 import SearchPage from "./pages/SearchPage.jsx";
 import ProgramDetailPage from "./pages/ProgramDetailPage";
-import LoginPage from"./pages/LoginPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx";
+import { useEffect } from "react";
+import axios from "axios";
 
-export default function App(){return <LoginPage/>;}
+export default function App() {
+  useEffect(() => {
+    axios
+      .post("/api/auth/login", {
+        email: "hong@company.com",
+        password: "900515",
+      })
+      .then((res) => {
+        console.log("로그인 성공:", res.data);
+      })
+      .catch((err) => {
+        console.error("로그인 실패", err);
+      });
+  }, []);
+
+  return (
+    <div>
+      로그인 테스트 중...
+      <LoginPage />
+    </div>
+  );
+}
