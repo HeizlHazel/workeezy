0a7da239b03d68c3eccf9869312e7413eed37c94
baekkayoung <baekkayoung@naver.com>
2025-12-01 09:29:23 +0900

    :memo: App.jsx 충돌 수정

--- workeezy-react/src/App.jsx
+++ workeezy-react/src/App.jsx
@@ -2,11 +2,6 @@ import ReviewPage from "./pages/ReviewPage";
 import SearchPage from "./pages/SearchPage.jsx";
 import ProgramDetailPage from "./pages/ProgramDetailPage";
 
-<<<<<<< HEAD
-
-export default function App(){return <ProgramDetailPage/>;}
-=======
 export default function App() {
-  return <SearchPage />;
+  return <ProgramDetailPage />;
 }
->>>>>>> fece12d58aec6959d50d724e7e1450a83e2b7a90
