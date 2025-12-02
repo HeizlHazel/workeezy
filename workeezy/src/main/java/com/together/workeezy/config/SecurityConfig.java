1142d45de4c483c72f1b5ecc576585e49a16a682
HeizlHazel <hyejee0221@gmail.com>
2025-12-02 14:47:55 +0900

    :memo; Cors config 설정

--- workeezy/src/main/java/com/together/workeezy/config/SecurityConfig.java
+++ workeezy/src/main/java/com/together/workeezy/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.together.workeezy.config;
 
+import com.together.workeezy.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,15 +11,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-//    @Bean
+    //    @Bean
 //    public BCryptPasswordEncoder bCryptPasswordEncoder() { return new BCryptPasswordEncoder(); }
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(
@@ -30,13 +37,21 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll()
+                .cors(cors -> {
+                })              // ✔️ CORS 설정 활성화, CorsConfig에서 처리
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // JWT 방식에서는 서버가 세션을 만들지 않음
+                .authorizeHttpRequests(auth -> auth // 로그인만 허용, 나머지는 JWT 필요
+                        .requestMatchers("/", "/api/auth/login").permitAll()
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(login -> login.disable())
-                .httpBasic(basic -> basic.disable());
+                .formLogin(login -> login.disable()) // form 로그인 사용 안 함
+                .httpBasic(basic -> basic.disable()); // Basic Auth 사용 안 함
+
+        // JWT 필터 등록
+        // JWT 필터가 스프링 필터 체인 앞에서 토큰 인증 처리
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
