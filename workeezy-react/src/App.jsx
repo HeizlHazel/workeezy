c03f1515c8eaf60482f48d08f5a5e7ca1d7c24e9
baekkayoung <baekkayoung@naver.com>
2025-12-02 11:50:19 +0900

    ✨ feat: 예약 수정 폼 로직 구현 (DB 연동 전))

--- workeezy-react/src/App.jsx
+++ workeezy-react/src/App.jsx
@@ -3,7 +3,13 @@ import SearchPage from "./pages/SearchPage.jsx";
 import ProgramDetailPage from "./pages/ProgramDetailPage";
 import LoginPage from "./pages/LoginPage.jsx";
 import NewReservationForm from "./pages/NewReservationForm";
+import ModifyReservationPage from "./pages/ModifyReservationPage.jsx";
 
 export default function App() {
-  return <NewReservationForm />;
+  return (
+    <>
+      {/* <ModifyReservationPage /> */}
+      <NewReservationForm />
+    </>
+  );
 }
