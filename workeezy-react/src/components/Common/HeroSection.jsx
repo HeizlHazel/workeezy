ece3603a7dda3d267ace482584e0fada1856ce9f
HeizlHazel <hyejee0221@gmail.com>
2025-12-04 21:12:32 +0900

    💄 메인 content CSS 변경

--- workeezy-react/src/components/Common/HeroSection.jsx
+++ workeezy-react/src/components/Common/HeroSection.jsx
@@ -3,8 +3,15 @@ import "./HeroSection.css";
 
 export default function HeroSection() {
     return (
-        <div className="hero-section">
-            <SearchBar/>
-        </div>
+        <section className="section hero">
+            <div className="hero-content">
+                <h1 className="hero-title">Workeezy와 함께 워케이션 찾기</h1>
+                <p className="hero-subtitle">여행과 업무를 동시에, 새로운 경험을 시작하세요.</p>
+
+                <div className="hero-search">
+                    <SearchBar />
+                </div>
+            </div>
+        </section>
     );
 }
