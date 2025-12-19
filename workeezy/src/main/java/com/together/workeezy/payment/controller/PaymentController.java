bc1a3bfe8730df7729ceb73b52201b33d478c79f
Hyejee Kim <hyejee0221@gmail.com>
2025-12-19 14:18:38 +0900

    🔧 컨트롤러 매핑 추가

--- workeezy/src/main/java/com/together/workeezy/payment/controller/PaymentController.java
+++ workeezy/src/main/java/com/together/workeezy/payment/controller/PaymentController.java
@@ -8,9 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/payments")
@@ -32,5 +30,11 @@ public class PaymentController {
         // 3. 결과 반환
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/{reservationId}")
+    public ResponseEntity<PaymentConfirmResponse> getPayment() {}
+
+//    @GetMapping("/receipt/{reservationId}")
+//    public ResponseEntity<PaymentConfirmResponse> getPayment(@PathVariable("reservationId") String reservationId) {}
 
 }
